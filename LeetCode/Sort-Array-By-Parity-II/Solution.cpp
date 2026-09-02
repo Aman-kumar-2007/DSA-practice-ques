1a26dc7a5370e0d13a922044656ad4e4a69ed404
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-09-02 12:31:18 +0530

    Add solution for Sort Array By Parity II

--- LeetCode/Sort-Array-By-Parity-II/Solution.cpp
+++ LeetCode/Sort-Array-By-Parity-II/Solution.cpp
@@ -1,24 +1,15 @@
-1class Solution {
-2public:
-3    vector<int> sortArrayByParityII(vector<int>& nums) {
-4      int n = nums.size();
-5      stack<int> odd;
-6      stack<int> even;
-7      for(int i=0; i<n; i++){
-8        if(nums[i] % 2 != 0) odd.push(nums[i]);
-9        else even.push(nums[i]);
-10      }
-11
-12      for(int i=0; i<n; i++){
-13        if(i % 2 == 0){
-14          nums[i] = even.top();
-15          even.pop();
-16        } else {
-17          nums[i] = odd.top();
-18          odd.pop();
-19        }
-20      }
-21
-22      return nums;
-23    }
-24};
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int i = 0; int j = 1; int n = nums.size();
+        while (i < n && j < n) {
+            if(nums[i] % 2 == 0) i+=2;
+            else if (nums[j] % 2 == 1) j+=2;
+            else {
+                (swap(nums[i], nums[j]));
+                i+=2, j+=2;
+            }
+        }
+        return nums;
+    }
+};
