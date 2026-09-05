6e4b82f7736ba86a639bd2ae4487c4a3b45d2204
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-09-05 09:19:33 +0530

    Add solution for Smallest Stable Index II

--- LeetCode/Smallest-Stable-Index-II/Solution.cpp
+++ LeetCode/Smallest-Stable-Index-II/Solution.cpp
@@ -3,21 +3,21 @@
 3    int firstStableIndex(vector<int>& nums, int k) {
 4        int n = nums.size();
 5
-6        vector<int> prefix(n), suffix(n);
+6        vector<int> prefix(n);
 7        prefix[0] = nums[0];
-8        for (int i = 1; i < n; i++) {
-9            prefix[i] = max(prefix[i - 1], nums[i]);
+8        for(int i=1; i<n; i++){
+9            prefix[i] = max(prefix[i-1],nums[i]);
 10        }
-11        suffix[n - 1] = nums[n - 1];
-12        for (int i = n - 2; i >= 0; i--) {
-13            suffix[i] = min(suffix[i + 1], nums[i]);
-14        }
-15
-16        for (int i = 0; i < n; i++) {
-17            if (prefix[i] - suffix[i] <= k) {
-18                return i;
-19            }
+11
+12        vector<int> suffix(n);
+13        suffix[n-1] = nums[n-1];
+14        for(int i=n-2; i>=0; i--){
+15            suffix[i] = min(suffix[i+1],nums[i]);
+16        }
+17
+18        for(int i=0; i<n; i++){
+19            if(prefix[i] - suffix[i] <= k) return i;
 20        }
-21      return -1;
+21        return -1;
 22    }
 23};
