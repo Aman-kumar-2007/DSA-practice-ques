1e2fd86ab8bba490bab65b08cf7431068546ba9d
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-09-23 11:50:27 +0530

    Add solution for Range Sum of BST

--- LeetCode/Range-Sum-of-BST/Solution.cpp
+++ LeetCode/Range-Sum-of-BST/Solution.cpp
@@ -6,16 +6,20 @@
 6 *     TreeNode *right;
 7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
 8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13public:
-14    int rangeSumBST(TreeNode* root, int low, int high) {
-15      if(root == NULL) return 0;
-16      int x = 0;
-17      if(root->val >= low && root->val <= high) x = root->val;
-18      else x = 0;
-19      return x + rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high) ;
-20    }
-21};
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+10 * right(right) {}
+11 * };
+12 */
+13class Solution {
+14public:
+15    int rangeSumBST(TreeNode* root, int low, int high) {
+16        if (root == NULL)
+17            return 0;
+18        if (root->val > high)
+19            return rangeSumBST(root->left, low, high);
+20        if (root->val < low)
+21            return rangeSumBST(root->right, low, high);
+22        return root->val + rangeSumBST(root->left, low, high) +
+23               rangeSumBST(root->right, low, high);
+24    }
+25};
